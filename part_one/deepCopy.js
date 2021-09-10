da66fad276bff36901331ce8de99c6c46381888f
Adam <adamskoullos@gmail.com>
2021-09-10 20:46:08 +0100

    Add solution for deepClone

--- part_one/deepCopy.js
+++ part_one/deepCopy.js
@@ -1,18 +1,29 @@
+// Solution 1
+// Is Immutable but only clones the top layer
+// I reach for the spread whenever it suits
 const deepCopy = (sourceObj) => {
-  // your code here
+  return { ...sourceObj };
+};
+
+// Solution 2
+// A good option when deep cloning objects with nested properties
+// Note: Does not clone methods !
+// A powerful pattern but not suitable for the example object
+const deepCopyTwo = (sourceObj) => {
+  return JSON.parse(JSON.stringify(sourceObj));
 };
 
 // Example simple test case
 const source = {
   a: 1,
-  b: 'string',
+  b: "string",
   c: false,
 };
 
 const target = deepCopy(source);
-console.group('Set1');
-console.log('source ==>', source);
-console.log('target ==>', target);
+console.group("Set1");
+console.log("source ==>", source);
+console.log("target ==>", target);
 console.groupEnd();
 
 // Example more advanced test case
@@ -22,13 +33,13 @@ const source1 = {
     c: 1,
     d: 2,
     e: new Date(),
-    f: () => console.log('Hello World'),
+    f: () => console.log("Hello World"),
   },
 };
 const target1 = deepCopy(source1);
-console.group('Set2');
-console.log('source ==>', source1);
-console.log('target ===>', target1);
+console.group("Set2");
+console.log("source ==>", source1);
+console.log("target ===>", target1);
 console.groupEnd();
 
 // Feel free to show off different style test cases as you see fit
